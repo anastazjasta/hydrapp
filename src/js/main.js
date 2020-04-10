b8ae70d014dc5a208b6d32ae59c36be103de3f76
anastazjasta <aneta.dakalo@gmail.com>
2020-04-10 17:01:29 +0200

    fix: counter

--- src/js/main.js
+++ src/js/main.js
@@ -2,20 +2,25 @@
 
 // service worker registration - remove if you're not going to use it
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("serviceworker.js").then(
+      function (registration) {
+        // Registration was successful
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      function (err) {
+        // registration failed :(
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
   });
 }
 
 // place your code below
-
 
 const add = document.querySelector(".button--add-js");
 const remove = document.querySelector(".button--remove-js");
@@ -24,20 +29,26 @@ let counterView = document.querySelector(".glass__counter");
 
 counterView.innerHTML = counterValue;
 
-add.addEventListener('click', (e) => {
-  if (counterValue < 10) {
-    counterValue++;
-    console.log(counterValue);
-    counterView.innerHTML = counterValue;
-  }
-})
-
-remove.addEventListener('click', (e) => {
-  if (counterValue > 0) {
-  counterValue--;
-  console.log(counterValue);
+const key = new Date().toISOString().slice(0, 10);
+if (!key in localStorage) {
+  counterValue = 0;
+} else {
+  counterValue = localStorage.getItem(key);
   counterView.innerHTML = counterValue;
 }
-})
 
+add.addEventListener("click", (e) => {
+  if (counterValue < 10) {
+    counterValue++;
+    counterView.innerHTML = counterValue;
+    localStorage.setItem(key, counterValue);
+  }
+});
 
+remove.addEventListener("click", (e) => {
+  if (counterValue > 0) {
+    counterValue--;
+    counterView.innerHTML = counterValue;
+    localStorage.setItem(key, counterValue);
+  }
+});
